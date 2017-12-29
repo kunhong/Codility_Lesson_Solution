bd66ee5d4215995a633249095526d6fd15d8687e
홍건 <hongkun@hongui-macbook-pro.local>
2017-12-29 23:53:16 +0900

    Initial Commit

---
+++ Codility_Lesson/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Codility_Lesson
+//
+//  Created by Hong Kun on 2017. 12. 29..
+//  Copyright © 2017년 Hong Kun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
